693147108b735bdc0f38242bf32370c646377835
Nikolaos Vlachos <thepredatorvlachos@gmail.com>
2026-07-25 12:26:59 +0300

    feat: implement blinking led on pin PC13

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -95,7 +95,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13); // invert the current state of the pin
+	  HAL_Delay(500); // wait for 500 ms
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
